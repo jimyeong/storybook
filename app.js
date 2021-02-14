bbb83887548a63b77fe2b8eaba0a0acea27f57c0
jimyeong <idjjm92@gmail.com>
2021-02-14 20:30:04 +0900

    init passport google is set up

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const passport = require("passport");
+require("./models/User");
 require("./config/passport")(passport);
 const oauth = require("./routes/oauth");
+const keys = require("./config/keys");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+// mongoose connect
+mongoose.connect(keys.mongoURL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=>{
+    console.log("mongo is connected");
+}).catch(err => console.log("error: " + err));
+
+// session
+app.use(cookieParser());
+app.use(session({
+    secret: "jimmy",
+    resave: false,
+    saveUninitialized: false
+}));
+
+// passportmiddleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 app.get("/", (req, res)=>{
--- config/passport.js
+++ config/passport.js
@@ -1,17 +1,49 @@
 const GoogleStretegy = require("passport-google-oauth20").Strategy;
 const mongoose = require("mongoose");
 const keys = require("./keys");
+const User = mongoose.model("user");
 
-module.exports = function (passport){
+
+module.exports = function (passport) {
     passport.use(new GoogleStretegy({
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
         callbackURL: "/oauth/google/callback",
         proxy: true // https, error not gonna trrow
-    },  (accessToken , refreshToken, profile, done)=>{
-        console.log(accessToken);
-        console.log(profile);
-    }))
+    }, (accessToken, refreshToken, profile, done) => {
+        // console.log(accessToken);
+        // console.log(profile);
+        const image = profile.photos[0].value
+        console.log(image);
+        const newUser = {
+            googleID: profile.id,
+            firstname: profile.name.givenName,
+            familyname: profile.name.familyname,
+            email: profile.emails[0].value,
+            image: image
+        }
 
+        // check for existing user
+        User.findOne({
+            googleID: profile.id
+        }).then(user => {
+            if (user) {
+                done(null, user);
+            } else {
+                // create newUser
+                new User(newUser)
+                    .save()
+                    .then(user => done(null, user))
+            }
+        })
+
+    }))
+    passport.serializeUser((user, done)=>{
+        done(null, user.id);
+    })
+
+    passport.deserializeUser((id, done)=>{
+        User.findById(id).then(user=>done(null, user));
+    })
 }
 
